04a2e499f01b2d6ef50b1333a06f839342d1faee
Dang Xuan Thong <89197317+DangXuanThong@users.noreply.github.com>
2026-01-25 04:54:00 +0000

    Added fish and mongohud config files to `home.nix`

--- nix/home.nix
+++ nix/home.nix
@@ -4,23 +4,6 @@
   home.username = "penguin";
   home.homeDirectory = "/home/penguin";
 
-  # Import files from the current configuration directory into the Nix store,
-  # and create symbolic links pointing to those store files in the Home directory.
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # Import the scripts directory into the Nix store,
-  # and recursively generate symbolic links in the Home directory pointing to the files in the store.
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
   programs = {
     git = {
       enable = true;
@@ -38,10 +21,21 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     kdePackages.kate
-    flameshot
-    jetbrains.idea
     prismlauncher
+    jetbrains.idea
+    flameshot
   ];
+
+  home.file = {
+    # Fish config
+    ".config/fish" = {
+      source = ../config/fish;
+      recursive = true;
+    };
+
+    # MangoHud config
+    ".config/MangoHud".source = ../config/MangoHud;
+  };
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
