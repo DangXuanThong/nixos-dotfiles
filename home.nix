53345f23237cb656beee55c4a98a5275daff3600
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-01-27 18:08:05 +0700

    Updated hardware configuration to use absolute path

--- home.nix
+++ home.nix
@@ -23,7 +23,7 @@
     fish = {
       enable = true;
       shellAliases = {
-        nix-rebuild = "sudo nixos-rebuild switch --flake ~/.config/nixos-dotfiles";
+        nix-rebuild = "sudo nixos-rebuild switch --flake ~/.config/nixos-dotfiles --impure";
       };
     };
     ghostty.enableFishIntegration = true;
