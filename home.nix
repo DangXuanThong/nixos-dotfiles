917ddf55a4e4326c055d48c703e1b008487e3add
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-01-31 17:22:48 +0700

    Enabled automatic garbage collector

--- home.nix
+++ home.nix
@@ -11,6 +11,15 @@ let
 in
 
 {
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
   home.username = "penguin";
   home.homeDirectory = "/home/penguin";
   home.shell.enableFishIntegration = true;
@@ -139,13 +148,9 @@ in
     recursive = true;
   }) configs;
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "25.11";
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
