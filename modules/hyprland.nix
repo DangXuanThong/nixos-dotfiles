0724954371772491607642ff1b6429447d6d804c
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-05-25 22:26:26 +0700

    Install some neccesary hypr* packages

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -5,5 +5,24 @@
     enable = true;
     configType = "lua";
     systemd.enable = false;
+    settings = {
+      mod = {
+        _var = "SUPER";
+      };
+    };
   };
+
+  services = {
+    hyprpolkitagent.enable = true;
+    hypridle.enable = true;
+    hyprpaper.enable = true;
+  };
+
+  programs = {
+    hyprshot.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    nautilus
+  ];
 }
