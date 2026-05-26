3973d25828853927043dbdca1f819e725c3dbf90
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-05-26 15:47:55 +0700

    Added keybind to resize/move window when in float mode

--- config/hypr/binds.lua
+++ config/hypr/binds.lua
@@ -12,9 +12,11 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(menu))
 
 -- Window management
--- hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind("ALT + F4", hl.dsp.window.close())
-hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + F", hl.dsp.window.float({ action = "toggle" }))
+-- Move/resize windows with mainMod + LMB/RMB and dragging
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 -- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 
 -- Workspace management
