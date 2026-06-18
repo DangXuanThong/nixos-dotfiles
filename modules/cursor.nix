3d13d356493affb84f165098f3ff0efdaed04357
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-06-18 15:45:12 +0700

    Modified way to install the 2 cursor packs

--- modules/cursor.nix
+++ modules/cursor.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 let
+  cursorName = "MacTahoeCursor";
   macTahoeCursor = pkgs.stdenv.mkDerivation {
     name = "MacTahoeCursor";
     src = ../config/cursor;  # copies the whole cursor/ dir to the store
@@ -9,7 +10,7 @@ let
 
     buildPhase = ''
       bash $src/scripts/build-hyprcursor.sh $src/src/MacTahoeCursor $out/share/icons
-      bash $src/scripts/build-xcursor.sh $src/src/MacTahoeXCursor $out/share/icons/MacTahoeCursor
+      bash $src/scripts/build-xcursor.sh $src/src/MacTahoeXCursor $out/share/icons/${cursorName}
     '';
 
     installPhase = "true";
@@ -18,14 +19,20 @@ in
 
 {
   home.pointerCursor = {
-    name = "MacTahoeCursor";
-    size = 36;
+    name = cursorName;
     package = macTahoeCursor;
-    gtk.enable = true;
-    x11.enable = true;
-    hyprcursor = {
-      enable = true;
-      size = 36;
-    };
+  };
+
+  xdg.configFile = {
+    "gtk-4.0/settings.ini".text = ''
+      [Settings]
+      gtk-cursor-theme-name=${cursorName}
+      gtk-cursor-theme-size=36
+    '';
+    "gtk-3.0/settings.ini".text = ''
+      [Settings]
+      gtk-cursor-theme-name=${cursorName}
+      gtk-cursor-theme-size=36
+    '';
   };
 }
