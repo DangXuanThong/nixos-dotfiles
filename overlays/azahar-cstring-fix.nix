20c8534f12946057a11dae4e76c6b6198651893a
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2026-09-11 11:18:08 +0700

    Removed ported .nix files

--- overlays/azahar-cstring-fix.nix
+++
@@ -1,8 +0,0 @@
-final: prev: {
-  azahar = prev.azahar.overrideAttrs (old: {
-    postPatch = (old.postPatch or "") + ''
-      sed -i '1i #include <cstring>' src/audio_core/cubeb_sink.cpp
-      sed -i '1i #include <cstring>' src/audio_core/cubeb_input.cpp
-    '';
-  });
-}
